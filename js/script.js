48b62d5ea994a48daa6d2fb2dec2f142c7bbbf4f
aabor73 <aabor73@yandex.ru>
2023-12-09 16:57:28 +0300

    3 dey

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Initialize Swiper
+
+const swiperThumb = new Swiper('.gift__swiper--thumb', {
+   spaceBetween: 5,
+   slidesPerView: 12,
+   freeMode: true,
+   breakpoints: {
+      320: {
+         slidesPerView: 5,
+         spaceBetween: 12,
+      },
+      1141: {
+         spaceBetween: 16,
+         slidesPerView: 6,
+      }
+   }
+ });
+
+const swiperMain = new Swiper('.gift__swiper--card', {
+   spaceBetween: 16,
+   thumbs: {
+      swiper: swiperThumb,
+   }
+ });
